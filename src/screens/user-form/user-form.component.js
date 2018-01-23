b7e8e3c57685a5bef37130c1010ec290e11a9d95
Fatin Haque <2547050+fhaque@users.noreply.github.com>
2018-01-23 13:16:57 -0500

    blah

--- src/screens/user-form/user-form.component.js
+++ src/screens/user-form/user-form.component.js
@@ -1,8 +1,9 @@
 /* @flow */
 import * as React from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
+import { withProps, compose } from 'recompose';
 
-import { withFormik, Field } from 'formik';
+import { withFormik, Field, Formik } from 'formik';
 
 const FormikTextInput = (props) => {
   const { onChangeText, name, ...otherProps } = props;
@@ -14,7 +15,7 @@ const FormikTextInput = (props) => {
   );
 };
 
-const InnerForm = ({
+let InnerForm = ({
   values,
   errors,
   touched,
@@ -30,10 +31,12 @@ const InnerForm = ({
       onChangeText={setFieldValue}
     />
     <Field name="email" render={(renderProps) => {console.log("Field Props", renderProps); return <TextInput />} } />
-    <Field render={() => <Button title="Submit" onPress={handleSubmit}/>} />
-    {/* <Button title="Submit" onPress={handleSubmit} /> */}
+    {/* <Field render={() => <But
+    ton title="Submit" onPress={handleSubmit}/>} /> */}
+    <Button title="Submit" onPress={handleSubmit} />
   </View>
 );
+
 
 const FormContainer = withFormik({
   handleSubmit(values, formikBag) {
@@ -41,9 +44,18 @@ const FormContainer = withFormik({
   }
 })(InnerForm);
 
-export const UserFormComponent = (): React.Node  => (
-  <View>
-    <Text>User Form</Text>
-    <FormContainer />
-  </View>
-);
+export class UserFormComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this._form = undefined;
+  }
+  render() {
+    return (
+      <View>
+        <Text>User Form</Text>
+        <FormContainer ref={c => { console.log('ref', c); this._form = c; } } />
+        <Button title="print to console" onPress={() => console.log('ref',this._form)} />
+      </View>
+    );
+  }
+}
